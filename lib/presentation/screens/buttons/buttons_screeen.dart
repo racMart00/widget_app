44eb31e99be2db75f728526e51022790bece4234
racmart00 <racmart00@gmail.com>
2024-03-20 14:33:21 -0400

    011 Boton Personalizado

--- lib/presentation/screens/buttons/buttons_screeen.dart
+++ lib/presentation/screens/buttons/buttons_screeen.dart
@@ -84,7 +84,7 @@ class _ButtonsView extends StatelessWidget {
               label: const Text('Text Icon')
             ),
 
-            //TODO: Custom Button
+            const CustomButton(),
 
             IconButton(
               onPressed: (){},
@@ -100,6 +100,34 @@ class _ButtonsView extends StatelessWidget {
             ),
 
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              'Hola mundo',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
         ),
       ),
     );
